653aeb4e56e6f592b86f3889a6e72d4fc9b2e4b0
tinka <tinkalouis86@gmail.com>
2026-07-06 10:22:59 +0200

    ci: ajout pipeline GitHub Actions (tests PHPUnit + PostgreSQL)

---
+++ tests/HomePageTest.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomePageTest extends WebTestCase
+{
+    public function testHomePageIsUp(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+    }
+}
---
+++ tests/KernelBootTest.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class KernelBootTest extends KernelTestCase
+{
+    public function testKernelBoots(): void
+    {
+        self::bootKernel();
+
+        $this->assertTrue(self::getContainer()->has('router'));
+    }
+}
